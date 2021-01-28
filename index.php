251836b0aed340c642721c3ae2119bd7acd0faaf
samybyy93 <78159715+samybyy93@users.noreply.github.com>
2021-01-28 10:15:48 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> PROJET RESTAURANT</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Castoro&family=Patua+One&display=swap" rel="stylesheet">
+		<script src="https://kit.fontawesome.com/e3459eeef5.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<main>
+	<section class="banner">
+		<header id="navbar">
+			<div class="header_restor">
+				<div class="btn_header">
+					<div class="btn btn_bottom">
+						<a href=""> make reservation</a>
+					</div>
+					<div class="btn">
+						<a href="">order takeout</a> 
+					</div>
+				</div>
+				<div class="logo">
+					<a href=""><img src="IMAGES/logo.png">
+					</a>
+				</div>
+				<div class="menu_hamburger" id="hamburger">
+					<i class="fas fa-bars"></i>
+				</div>
+			</div>
+
+			<div class="menu_header">
+				<ul class="liste_menu slide_header">
+					<li><a href="">MENU</a></li>
+					<li><a href="">OUR STORY</a></li>
+					<li><a href="">contact us</a></li>
+					<li><a href="">GET IN TOUCH</a></li>
+				</ul>
+			</div>
+		</header>
+		<div class="banner_flex">
+			<div class="text_banner">
+				<h1>now open</h1>
+			</div>
+		</div>
+		<div class="social_banner">
+			<a href=""><i class="fab fa-instagram"></i></a>
+			<a href=""><i class="fab fa-facebook-f"></i></a>
+		</div>
+	</section>
+	<section class="about">
+		<div class="container container_one">
+			<div class="about_flex">
+				<div class="about_title">
+					<div class="title">
+						<h2>NEIGHBORHOOD RESTAURANT AND BAR</h2>
+					</div>
+					<div class="title_pic">
+						<img src="https://images.squarespace-cdn.com/content/v1/5b48ae2d96e76ffeb9871ba6/1562451129438-DUZVQW0WPHIVGFVDGIKX/ke17ZwdGBToddI8pDm48kB-tLEw73WmMwJmka564Gbd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iXS6XmVv7bUJ418E8Yoc1jLe3hY13oPv3JRXfK1wnLgkt1UW1MCs_aoZUJjUa_4fg/PICK1-2T5A8775.jpg">
+					</div>
+				</div>
+				<div class="about_text">
+					<div class="text_pic">
+						<img src="IMAGES/logo.png">
+					</div>
+					<div class="text">
+						<p>Not a hipster cafe, not quite fine dining, Restore Public House lovingly crafts regional dishes, made from scratch using local and seasonal ingredients. Our goal is to provide an atmosphere where laughing out loud with friends and sampling chef-driven fare go hand-in-hand.
+						.</p>
+					</div>
+						<div class="btn btn_h3">
+							<a href=""> make reservation</a>
+						</div>
+				</div>
+			</div>
+				<div class="container_reservation">
+					<h2>
+						BIG FLAVORS SMALL BUILDING
+					</h2>
+					<p>
+						Book your table, relax and let us take care of the details.
+					</p>
+					<div class="btn btn_h3">
+						<a href=""> make reservation</a>
+					</div>
+				</div>
+		</div>
+			<div class="container container_two">
+				<div class="btn_flex">
+					<div class="btn_menu">
+						<a href="" class="btn btn_active">all</a>
+					</div>
+					<div class="btn_menu">
+						<a href="" class="btn">breakfast</a>
+					</div>
+					<div class="btn_menu">
+						<a href="" class="btn">lunch</a>
+					</div>
+					<div class="btn_menu">
+						<a href="" class="btn">dinner</a>
+					</div>
+				</div>
+				<div class="menu_flex">
+					<div class="box01">
+						<img src="https://images.unsplash.com/photo-1479388891567-3c137b61de97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80">
+					</div>
+					<div class="box01">
+						<img src="https://images.unsplash.com/photo-1566749249285-1798d7a0e470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60">
+					</div>
+					<div class="box01">
+						<img src="https://images.unsplash.com/photo-1533622597524-a1215e26c0a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60">
+					</div>
+					<div class="box01">
+						<img src="https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60">
+					</div>
+					<div class="box01">
+						<img src="https://images.unsplash.com/photo-1579954115563-e72bf1381629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60">
+					</div>
+					<div class="box01">
+						<img src="https://images.unsplash.com/photo-1566363711363-70f57f04aba7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60">	
+					</div>
+				</div>
+			</div>
+	<footer>
+		<div class="footer_flex">
+			<div class="horaire_resto">
+				<div class="logo_footer logo">
+						<a href=""><img src="IMAGES/logo.png">
+						</a>
+				</div>
+				<div class="text_footer">
+					<h2>1810 State Street, La Crosse, WI</h2>
+				</div>
+				<table>
+					<tr>
+					    <td>Monday</td>
+					    <td>CLOSED</td>
+					</tr>
+					<tr>
+					    <td>Tuesday</td>
+					    <td>CLOSED</td>
+					</tr>
+					<tr>
+					    <td>Wednesday</td>
+					    <td>4 pm - 9:00 pm</td>
+					</tr>
+					<tr>
+					    <td>Thursday</td>
+					    <td>4 pm - 9:00 pm</td>
+					</tr>
+					<tr>
+					    <td>Friday</td>
+					    <td>4 pm - 9:00 pm</td>
+					</tr>
+					<tr>
+					    <td>Saturday</td>
+					    <td>4 pm - 9:00 pm</td>
+					</tr>
+					<tr>
+					    <td>Sunday</td>
+					    <td>11 am - 9:00 pm</td>
+					</tr>
+				</table>
+			</div>
+			<div class="slider">
+
+			 <div class="item">
+			 	<img src="https://www.luxxu.net/blog/wp-content/uploads/2018/11/5-Stylish-Inspiring-Restaurant-Designs-01.jpg">
+			 </div>
+			  <div class="item">
+			 	<img src="https://portal.restomontreal.ca/walter-bistro/gallery/images/11__396-2019-01-24.jpg">
+			 </div>
+			  <div class="item">
+			 	<img src="https://belleetboeuf.com/wp-content/uploads/2019/02/belle-boeuf-restaurant-montreal-centre-ville-00.jpg">
+			 </div>
+			  <div class="item">
+			 	<img src="https://images.squarespace-cdn.com/content/v1/58dc24759de4bb47fd28d48c/1584889454151-Q1Y4ZWNX9MBHEATBI3UV/ke17ZwdGBToddI8pDm48kJK4Mm1kch8SFO9ZNkN1NT97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmFk_H6M1tkD9NpL7mXac0oVSXdFfjxR5AjcLwGSebOiGBsFzzcw3xKxvyC_6CFFG_/IMG_20190419_154039_817.jpg">
+			 </div>
+			  <div class="item">
+			 	<img src="https://belleetboeuf.com/wp-content/uploads/2019/02/belle-boeuf-restaurant-boisbriand-08.jpg">
+			 </div>
+			  <div class="item">
+			 	<img src="https://belleetboeuf.com/wp-content/uploads/2019/02/belle-boeuf-restaurant-vaudreuil-06.jpg">
+			 </div>
+			</div>
+
+			
+
+		</div>
+		<div class="header_restor footer_padd">
+			<div class="">
+			<p>Copyright &copy; All right reserved This tempalte is made by samybyy</p>
+			</div>
+			<ul class="header_restor">
+				<a href=""><li>Order Online <span>.</span></li></a>
+				<a href=""><li>Make Reservation <span>.</span></li></a>
+				<a href=""><li>The Menu <span>.</span></li></a>
+				<a href=""><li>Our Story <span>.</span></li></a>
+				<a href=""><li>get in touch<span></span></li></a>
+			</ul>
+			<div class="social_footer">
+				<i class="fab fa-instagram"></i>
+				<i class="fab fa-facebook-f"></i>
+			</div>
+		</div>
+	</footer>
+	</section>
+</main>
+
+<script type="text/javascript">
+	var prevScrollpos = window.pageYOffset;
+	window.onscroll = function() {
+	var currentScrollPos = window.pageYOffset;
+	  if (prevScrollpos > currentScrollPos) {
+	    document.getElementById("navbar").style.top = "0";
+	  } else {
+	    document.getElementById("navbar").style.top = "-150px";
+	  }
+	  prevScrollpos = currentScrollPos;
+	}
+</script>
+
+<script src = "script/jquery-min.js"></script>
+<script src = "script/script.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+ <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+ <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+
+ <script type="text/javascript">
+ 	$(document).ready(function(){
+
+
+ 	 $('.slider').slick({
+     slidesToShow: 3,
+     slidesToScroll: 1,
+     autoplay: true,
+     infinite: true
+ });
+
+
+ 	});//ready function
+
+ </script>
+</body>
+</html>
